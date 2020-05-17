d7ceb22b439c49954ebe227599881d4ad6040bc2
Tae Hee Choi <tchoi@nitinol.internal>
2020-05-17 13:48:15 -0700

    Updates

---
+++ precache-manifest.4fe384709b5c251e59629bf79afca0f5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed47b4ccafa2d4a4cf16e55589438674",
+    "url": "/quiz-for-preschooler/index.html"
+  },
+  {
+    "revision": "8ca4d12bde0415e982cf",
+    "url": "/quiz-for-preschooler/static/css/main.ad504b64.chunk.css"
+  },
+  {
+    "revision": "870b9032cb9c222b099c",
+    "url": "/quiz-for-preschooler/static/js/2.9780aaa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-for-preschooler/static/js/2.9780aaa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca4d12bde0415e982cf",
+    "url": "/quiz-for-preschooler/static/js/main.690eaf03.chunk.js"
+  },
+  {
+    "revision": "e9ff9e80e25561ff33b5",
+    "url": "/quiz-for-preschooler/static/js/runtime-main.6414aef8.js"
+  },
+  {
+    "revision": "3b976236cd2c64a29c835b2419ed4ae0",
+    "url": "/quiz-for-preschooler/static/media/ambulance.3b976236.png"
+  },
+  {
+    "revision": "b1c3bf0999e4bf983010c7825ae2a5b6",
+    "url": "/quiz-for-preschooler/static/media/crane.b1c3bf09.png"
+  },
+  {
+    "revision": "be216c958194142392141a2c5468a66f",
+    "url": "/quiz-for-preschooler/static/media/firetruck.be216c95.png"
+  },
+  {
+    "revision": "b2449a4e6f28631817119aef0130e143",
+    "url": "/quiz-for-preschooler/static/media/police.b2449a4e.png"
+  }
+]);
